de0348ca312dd2f48625506ccec80af18a88f16b
amanmulani09 <mulaniaman0504@gmail.com>
2023-09-07 10:48:39 +0530

    initilized project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NameBadge from './component/NameBadge'
+function App() {
+
+  return (
+    <>
+      <h1>hello World</h1>
+      <NameBadge name='aman' />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/NameBadge.tsx
@@ -0,0 +1,8 @@
+
+const NameBadge = (props:{name:string}) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+export default NameBadge
